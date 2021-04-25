3cf2f227a9bbd952d304f95695b23ee9be099727
Mikayla <mikaylabryant25@gmail.com>
2021-04-25 13:18:42 -0400

    Removed test files and updated styling of privacy switch

--- src/components/SearchSongs/Test3.js
+++
@@ -1,8 +0,0 @@
-// Test 3 - hip hop with offset
-
-/*
-1. Fly by NJ - matches somewhat, but is in another language...
-2. Wish by NJ - matches somewhat but is by the same artist as before
-
-After reviewing the rest of the results, it is pretty much the same as without the offset (Test 1)
-*/
