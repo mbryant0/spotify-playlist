b4ff5ec8c7b8ef1d98fa73829cfdedd35a584c00
Mikayla <mikaylabryant25@gmail.com>
2021-05-01 00:39:30 -0400

    Updated favicon

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
