1183ed7500a5763fb3745dab2a9d02cbfc08e50b
Mikayla Bryant <mikaylabryant25@gmail.com>
2021-05-19 15:45:40 -0400

    Removed backup code comment

--- client/src/UseYupValidation.js
+++ client/src/UseYupValidation.js
@@ -31,40 +31,5 @@ const useYupValidation = (validationSchema) =>
     },
     [validationSchema]
   );
-/* const [newValue, setNewValue] = useState(initialState);
-  const [disabled, setDisabled] = useState(true);
-  const [errorMessages, setErrorMessages] = useState(errorMessageShape);
 
-  const setFormErrors = (name, value) => {
-    yup
-      .reach(schema, name)
-      .validate(value, { strict: true })
-      .then(() => {
-        setErrorMessages({ ...errorMessages, [name]: '' });
-      })
-      .catch((err) => {
-        setErrorMessages({ ...errorMessages, [name]: 'error' });
-      });
-  };
-
-  useEffect(() => {
-    schema.isValid(newValue).then((valid) => setDisabled(!valid));
-    console.log(schema);
-  });
-
-  return {
-    newValue,
-    setNewValue,
-    bind: {
-      onChange: (e) => {
-        const { checked, value, name, type } = e.target;
-        const updatedInfo = type === 'checkbox' ? checked : value;
-        setNewValue({ ...newValue, [name]: updatedInfo });
-        setFormErrors(name, updatedInfo);
-      },
-    },
-    disabled,
-  };
-};
- */
 export default useYupValidation;
